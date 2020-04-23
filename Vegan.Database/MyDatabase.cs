85edd6a296ed939a121f612a7099ee617d9b6836
elprs <eleni.parisi.sunshine@gmail.com>
2020-04-23 10:17:34 +0300

    Added connection string, MyDatabase class, and  AutomaticMigrationsEnabled = true;

---
+++ Vegan.Database/MyDatabase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace Vegan.Database
+{
+   public  class MyDatabase : DbContext
+    {
+        public MyDatabase() : base("Connection")
+        {
+
+        }
+    }
+}
