bc71423a1ec364fe0a014027dbb85f651cc13e28
elprs <eleni.parisi.sunshine@gmail.com>
2020-04-24 12:05:40 +0300

    product home

---
+++ Vegan.Entities/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vegan.Entities
+{
+    public abstract class Product
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public decimal Price { get; set; }
+    }
+}
