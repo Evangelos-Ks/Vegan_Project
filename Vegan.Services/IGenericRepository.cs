936fc041b9d6b8a6060b0c347b690f30aef75306
Evangelos Koutsogiorgos <evangkouts@gmail.com>
2020-04-29 09:59:35 +0100

    Insert IGenericRepository

---
+++ Vegan.Services/IGenericRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vegan.Services
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        T GetByID(object id);
+        void Insert(T obj);
+        void Update(T obj);
+        void Delete(object id);
+        void Save();
+    }
+}
