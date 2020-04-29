40a383757ec74be9d1759a5c46aaf29865aa2e0b
Evangelos Koutsogiorgos <evangkouts@gmail.com>
2020-04-29 10:54:03 +0100

    Add IUnitOfWork

---
+++ Vegan.Services/IUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace Vegan.Services
+{
+    public interface IUnitOfWork<out TdbContext> where TdbContext : DbContext, new()
+    {
+        TdbContext Context { get; }
+        void CreateTransaction();
+        void Commit();
+        void Rollback();
+        void Save();
+    }
+}
