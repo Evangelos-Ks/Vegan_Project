77989618bfa2da256ff137166b997d6fa5bfade5
elprs <eleni.parisi.sunshine@gmail.com>
2020-05-01 20:24:28 +0300

    Changed EssentialOil Ingredient properties and changes equivelant configuration

--- Vegan.Entities/Home/EssentialOil.cs
+++ Vegan.Entities/Home/EssentialOil.cs
@@ -8,7 +8,8 @@ namespace Vegan.Entities.Home
 {
     public partial class EssentialOil : Home
     {
-        public string Ingredient { get; set; }
+        public string NameOfIngredient { get; set; }
+        public string ValueOfIngredient { get; set; }
         public BottleSize Size { get; set; }
 
        // https://www.the-apothecary.ca/Synergies-Blends_c_19.html
