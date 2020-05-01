87c36f54964b7e5dd50a4806c980dda1ec8772ca
Evangelos Koutsogiorgos <evangkouts@gmail.com>
2020-05-01 12:04:29 +0100

    Delete old test Controllers

---
+++ Vegan.Web/Controllers/TestControllers/TestUFaceCreamController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Vegan.Web.Controllers.TestControllers
+{
+    public class TestUFaceCreamController : Controller
+    {
+        // GET: TestUFaceCream
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
