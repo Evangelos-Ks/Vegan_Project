94995cd84a2ad25dd98792880a107a3c68c5037e
Evangelos Koutsogiorgos <evangkouts@gmail.com>
2020-05-02 11:39:40 +0100

    Deleted old Vegan.Web

--- Vegan.Web/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace Vegan.Web.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- Vegan.Web/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Vegan.Web.WebApiApplication" Language="C#" %>
