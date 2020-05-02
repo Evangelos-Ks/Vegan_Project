48e2372cb61ea100aaf60a9eecd09fc51a0bbd31
Evangelos Koutsogiorgos <evangkouts@gmail.com>
2020-05-02 11:47:54 +0100

    Created new Vegan.Web

---
+++ Vegan.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Vegan.Web.Startup))]
+namespace Vegan.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
