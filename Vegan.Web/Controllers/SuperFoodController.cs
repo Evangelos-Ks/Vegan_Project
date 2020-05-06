c46df5c937b35701cfc052266b7095279265dcdc
elprs <eleni.parisi.sunshine@gmail.com>
2020-05-06 09:18:46 +0300

    superFood

---
+++ Vegan.Web/Controllers/SuperFoodController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vegan.Database;
+using Vegan.Entities.Supplement;
+using Vegan.Services;
+using System.ComponentModel.DataAnnotations;
+
+namespace Vegan.Web.Controllers.TestControllers
+{
+    public class SuperFoodController : Controller
+    {
+        //===================================== Fields =====================================================================
+        private UnitOfWork unitOfWork = new UnitOfWork(new MyDatabase());
+
+
+        //private GenericRepository<SuperFood> repository;
+
+        //===================================== Constructor ================================================================
+        //public SuperFoodController()
+        //{
+        //    repository = new GenericRepository<SuperFood>(unitOfWork);
+        //}
+
+        //===================================== Methods ====================================================================
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View(unitOfWork.SuperFoods.GetAll());
+        }
+
+        [HttpGet]
+        public ActionResult AddSuperFood()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddSuperFood(SuperFood model)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    unitOfWork.SuperFoods.Add(model);
+                    unitOfWork.Complete();
+                    unitOfWork.Dispose();
+                    return RedirectToAction("Index", "SuperFood");
+                }
+            }
+            catch (Exception ex)
+            {
+                //TODO: We want to show an error message
+                return View();
+            }
+            return View();
+        }
+
+        public ActionResult DetailsSuperFood(int productId)
+        {
+            return View(unitOfWork.SuperFoods.GetById(productId));
+        }
+
+        [HttpGet]
+        public ActionResult EditSuperFood(int productId)
+        {
+            return View(unitOfWork.SuperFoods.GetById(productId));
+        }
+
+        [HttpPost]
+        public ActionResult EditSuperFood(SuperFood model)
+        {
+            if (ModelState.IsValid)
+            {
+                unitOfWork.SuperFoods.Edit(model);
+                unitOfWork.Complete();
+                unitOfWork.Dispose();
+                return RedirectToAction("Index", "SuperFood");
+            }
+            else
+            {
+                return View(model);
+            }
+        }
+
+        [HttpGet]
+        public ActionResult DeleteSuperFood(int productId)
+        {
+            return View(unitOfWork.SuperFoods.GetById(productId));
+        }
+
+        [HttpPost, ActionName("DeleteSuperFood")]
+        public ActionResult Delete(int productId)
+        {
+
+            var product = unitOfWork.SuperFoods.GetById(productId);
+            unitOfWork.SuperFoods.Delete(product);
+            unitOfWork.Complete();
+            unitOfWork.Dispose();
+            return RedirectToAction("Index", "SuperFood");
+        }
+    }
+}
