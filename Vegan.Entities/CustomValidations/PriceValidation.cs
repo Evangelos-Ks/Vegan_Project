2c8ddd78f604075474d3d51d02a22b76ea20ca10
elprs <eleni.parisi.sunshine@gmail.com>
2020-05-06 06:50:46 +0300

    validations and correcting image in delete action method in product

---
+++ Vegan.Entities/CustomValidations/PriceValidation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vegan.Entities.CustomValidations
+{
+   public class PriceValidation
+    {
+        public static ValidationResult ValidationGreaterOrEqualToZero(decimal value, ValidationContext context)
+        {
+            bool isValid = true;
+            if (value < 0m)
+            {
+                isValid = false;
+            }
+
+            if (isValid)
+                return ValidationResult.Success;
+            else
+                return new ValidationResult(string.Format("The field {0} must be equal to or greater than 0. ", context.MemberName, new List<string> { context.MemberName }));
+        }
+    }
+}
