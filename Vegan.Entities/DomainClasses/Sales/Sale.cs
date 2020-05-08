5c375a5b98e03c6542902819875da247d500d29b
elprs <eleni.parisi.sunshine@gmail.com>
2020-05-08 15:04:28 +0300

    Complete Sale class

--- Vegan.Entities/DomainClasses/Sales/Sale.cs
+++ Vegan.Entities/DomainClasses/Sales/Sale.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,7 +12,7 @@ namespace Vegan.Entities.DomainClasses
     {
         //====================================== Fields ========================================================
 
-        protected int SaleId;
+        public int SaleId { get; set; }
         protected decimal paymentAmount;
         public int PaymentChoice { get; set; }
 
