9d91c5e642183195df94830d6424c94782f0e211
Evangelos Koutsogiorgos <evangkouts@gmail.com>
2020-05-09 12:43:55 +0100

    Added Candle controller and CRUD views

---
+++ Vegan.Web/Controllers/CandleController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vegan.Database;
+using Vegan.Entities.Home;
+using Vegan.Services;
+
+namespace Vegan.Web.Controllers
+{
+    public class CandleController : Controller
+    {
+        //===================================== Fields =====================================================================
+        private UnitOfWork unitOfWork = new UnitOfWork(new MyDatabase());
+
+        //===================================== Methods ====================================================================
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View(unitOfWork.Candles.GetAll());
+        }
+
+        [HttpGet]
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Add(Candle model)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    unitOfWork.Candles.Add(model);
+                    unitOfWork.Complete();
+                    unitOfWork.Dispose();
+                    return RedirectToAction("Index", "Candle");
+                }
+            }
+            catch (Exception ex)
+            {
+                //TODO: We want to show an error message
+                return View();
+            }
+            return View();
+        }
+
+        public ActionResult Details(int productId)
+        {
+            return View(unitOfWork.Candles.GetById(productId));
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int productId)
+        {
+            return View(unitOfWork.Candles.GetById(productId));
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Candle model)
+        {
+            if (ModelState.IsValid)
+            {
+                unitOfWork.Candles.Edit(model);
+                unitOfWork.Complete();
+                unitOfWork.Dispose();
+                return RedirectToAction("Index", "Candle");
+            }
+            else
+            {
+                return View(model);
+            }
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int productId)
+        {
+            return View(unitOfWork.Candles.GetById(productId));
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeletePost(int productId)
+        {
+
+            var product = unitOfWork.Candles.GetById(productId);
+            unitOfWork.Candles.Delete(product);
+            unitOfWork.Complete();
+            unitOfWork.Dispose();
+            return RedirectToAction("Index", "Candle");
+        }
+    }
+}
