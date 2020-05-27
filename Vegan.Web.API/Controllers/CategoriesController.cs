6c4b44dad8c657bfeaa79cda464b47131f7cafd0
Evangelos Koutsogiorgos <evangkouts@gmail.com>
2020-05-27 16:28:29 +0100

    Cleaned code

--- Vegan.Web.API/Controllers/CategoriesController.cs
+++ Vegan.Web.API/Controllers/CategoriesController.cs
@@ -16,10 +16,9 @@ namespace Vegan.Web.API.Controllers
 {
     public class CategoriesController : ApiController
     {
-        // GET: api/Categories
         private UnitOfWork unitOfWork = new UnitOfWork(new MyDatabase());
 
-        // GET: api/Products
+        // GET: api/Categories
         public IEnumerable<IEnumerable<Product>> GetProducts()
         {
             List<IEnumerable<Product>> allCategories = new List<IEnumerable<Product>>();
@@ -31,98 +30,5 @@ namespace Vegan.Web.API.Controllers
 
             return allCategories;
         }
-
-        //// GET: api/Categories/5
-        //[ResponseType(typeof(Product))]
-        //public IHttpActionResult GetProduct(int id)
-        //{
-        //    Product product = db.Products.Find(id);
-        //    if (product == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    return Ok(product);
-        //}
-
-        //// PUT: api/Categories/5
-        //[ResponseType(typeof(void))]
-        //public IHttpActionResult PutProduct(int id, Product product)
-        //{
-        //    if (!ModelState.IsValid)
-        //    {
-        //        return BadRequest(ModelState);
-        //    }
-
-        //    if (id != product.Id)
-        //    {
-        //        return BadRequest();
-        //    }
-
-        //    db.Entry(product).State = EntityState.Modified;
-
-        //    try
-        //    {
-        //        db.SaveChanges();
-        //    }
-        //    catch (DbUpdateConcurrencyException)
-        //    {
-        //        if (!ProductExists(id))
-        //        {
-        //            return NotFound();
-        //        }
-        //        else
-        //        {
-        //            throw;
-        //        }
-        //    }
-
-        //    return StatusCode(HttpStatusCode.NoContent);
-        //}
-
-        //// POST: api/Categories
-        //[ResponseType(typeof(Product))]
-        //public IHttpActionResult PostProduct(Product product)
-        //{
-        //    if (!ModelState.IsValid)
-        //    {
-        //        return BadRequest(ModelState);
-        //    }
-
-        //    db.Products.Add(product);
-        //    db.SaveChanges();
-
-        //    return CreatedAtRoute("DefaultApi", new { id = product.Id }, product);
-        //}
-
-        //// DELETE: api/Categories/5
-        //[ResponseType(typeof(Product))]
-        //public IHttpActionResult DeleteProduct(int id)
-        //{
-        //    Product product = db.Products.Find(id);
-        //    if (product == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    db.Products.Remove(product);
-        //    db.SaveChanges();
-
-        //    return Ok(product);
-        //}
-
-        //protected override void Dispose(bool disposing)
-        //{
-        //    if (disposing)
-        //    {
-        //        db.Dispose();
-        //    }
-        //    base.Dispose(disposing);
-        //}
-
-        //private bool ProductExists(int id)
-        //{
-        //    return db.Products.Count(e => e.Id == id) > 0;
-        //}
     }
 }
