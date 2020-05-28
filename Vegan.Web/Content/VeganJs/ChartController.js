1d0cac578fffe3f9d90292d0f9bf6f373248c998
elprs <eleni.parisi.sunshine@gmail.com>
2020-05-28 22:00:45 +0300

    javascript in ChartController

--- Vegan.Web/Content/VeganJs/ChartController.js
+++ Vegan.Web/Content/VeganJs/ChartController.js
@@ -5,60 +5,46 @@
         var GetData = function () {
             $http.get(ordersURL)
                 .then(function (response) {
-                    console.log(response.data[0]);
-                    for (var i = 0; i < response.length - 1; i++) {
-                        $scope.Orders = response.data;  // sosto??
-                        /*  $scope.Orders += response.data[0];*/
-                        console.log(response);
+
+                    //Declare the arrays
+                    var yearArray = [];
+                    var totalArray = [];
+                   
+                    for (var i = 0; i < response.data.length; i++) {
+                        $scope.Orders = response.data;
+                        console.log(response.data);
+                        console.log(response.data[i].Total);
+
+                        //Make year into an int
+                        var year = yearArray.push(parseInt(response.data[i].OrderStamp.split("-")[0]));
                     }
-                })
+                    console.log(yearArray);
+
+                    //Find the unique years, put them in an array and sort them
+                    function onlyUnique(value, index, self) {
+                        return self.indexOf(value) === index;
+                    }
+                    var unique = yearArray.filter(onlyUnique);
+                    unique.sort();
+                    $scope.years = unique;
+
+                    var yearlyTotalArray = [];
+                    //Check the orders for every year
+                    for (var t = 0; t < unique.length; t++) {
+                        //Calculate the total for every year
+                        var yearlyTotal = 0;
+                        for (var i = 0; i < response.data.length; i++) {
+                            if (unique[t] === parseInt(response.data[i].OrderStamp.split("-")[0])) {
+                                yearlyTotal = yearlyTotal + response.data[i].Total;
+                            }
+                        }
+                        //Create an array with the totals
+                        yearlyTotalArray.push(yearlyTotal);
+                    }
+                    $scope.totalsPerYear = yearlyTotalArray;
+                });
         }
         GetData();
     };
-
   ChartProject.controller("ChartController", ChartController);
-
 })();
-
-    //                    $scope.Care = response.data[0];
-    //                    $scope.FoodHerbs = response.data[1];
-    //                    $scope.Homes = response.data[2];
-    //                    $scope.Supplements = response.data[3];
-    //                    $scope.AllProducts = $scope.Care.concat($scope.FoodHerbs).concat($scope.Homes).concat($scope.Supplements);
-
-    //                    //autocomplete
-    //                    autocomplete(document.getElementById("searchlight"), AutocompleteItems($scope.AllProducts));
-
-    //                    //search-btn
-    //                    document.getElementById("searchbutton").addEventListener("click", function () {
-    //                        var text = document.getElementById("searchlight").value;
-    //                        var products = [];
-
-    //                        for (var i in $scope.AllProducts) {
-
-    //                            if (text == $scope.AllProducts[i].Title) {
-    //                                if ($scope.AllProducts[i].SubCategory == "FaceCream") {
-    //                                    var controller = $scope.AllProducts[i].SubCategory + "s";
-    //                                }
-    //                                else {
-    //                                    var controller = $scope.AllProducts[i].SubCategory;
-    //                                }
-    //                                var detailsActionMethod = "Details" + $scope.AllProducts[i].SubCategory;
-    //                                var id = $scope.AllProducts[i].Id;
-    //                                var url = "/" + controller + "/" + detailsActionMethod + "?" + "productId=" + id;
-    //                                window.location.href = url;
-    //                            }
-    //                        }
-
-    //                    });
-    //                }, function myError(response) {
-    //                    console.log(response);
-    //                });
-
-
-                                                        //        }
-
-                                                        //        GetData();
-                                                        //    };
-
-                                                        //    ChartProject.controller("ChartController", ChartController);
