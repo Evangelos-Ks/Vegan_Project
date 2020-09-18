8c6d41c16dd5545ab9f77721845234f3c25ca442
Evangelos Koutsogiorgos <evangkouts@gmail.com>
2020-09-18 11:09:22 +0100

    Added IndexUserJs.Js with validation in min and max price filter

---
+++ Vegan.Web/Content/VeganJs/IndexUserJs.js
@@ -0,0 +1,7 @@
+﻿$(function () {
+    //Change the min attribute of maxPrice to be equal with the current minPrice 
+    $("#minPrice").change(() => {
+        var minPrice = $("#minPrice").val();
+        $("#maxPrice").attr("min", minPrice);
+    });
+});
