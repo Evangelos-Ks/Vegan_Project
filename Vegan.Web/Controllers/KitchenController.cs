9eee781c56038acc1740f2c52b160eb44d8da2ba
Evangelos Koutsogiorgos <evangkouts@gmail.com>
2020-09-23 14:02:53 +0100

    Kitchen IndexUser -> ready

--- Vegan.Web/Controllers/KitchenController.cs
+++ Vegan.Web/Controllers/KitchenController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Web.Mvc;
 using Vegan.Database;
 using Vegan.Entities.Home;
@@ -13,15 +16,70 @@ namespace Vegan.Web.Controllers.TestControllers
 
         //===================================== Methods ====================================================================
         [HttpGet]
-     // [Authorize(Roles = "Admins, Supervisors")]
         public ActionResult Index()
         {
             return View(unitOfWork.Kitchens.GetAll());
         }
+
         [HttpGet]
-        public ActionResult IndexUser()
+        public ActionResult IndexUser(string sortOrder, int? minPrice, int? maxPrice, int? page, int? pageSize)
         {
-            return View(unitOfWork.Kitchens.GetAll());
+            //Get all Candles
+            IEnumerable<Kitchen> kitchens = unitOfWork.Kitchens.GetAll().ToList();
+            unitOfWork.Dispose();
+
+            //Filter
+            ViewBag.MinPrice = minPrice;
+            ViewBag.MaxPrice = maxPrice;
+
+            if (minPrice != null)
+            {
+                kitchens = kitchens.Where(c => c.Price >= minPrice);
+            }
+
+            if (maxPrice != null)
+            {
+                kitchens = kitchens.Where(c => c.Price <= maxPrice);
+            }
+
+            //Sorting
+            ViewBag.TitleSortParam = string.IsNullOrEmpty(sortOrder) ? "title_desc" : "";
+            ViewBag.PriceSortParam = sortOrder == "price_asc" ? "price_desc" : "price_asc";
+
+            switch (sortOrder)
+            {
+                case "title_desc":
+                    kitchens = kitchens.OrderByDescending(c => c.Title);
+                    break;
+                case "price_asc":
+                    kitchens = kitchens.OrderBy(c => c.Price);
+                    break;
+                case "price_desc":
+                    kitchens = kitchens.OrderByDescending(c => c.Price);
+                    break;
+                default:
+                    kitchens = kitchens.OrderBy(c => c.Title);
+                    break;
+            }
+
+            //Paging
+            ViewBag.CurrentSort = sortOrder;
+
+            int pSize = pageSize ?? 3;
+            int pageNumber = page ?? 1;
+
+            ViewBag.PageSize = new List<SelectListItem>()
+            {
+             new SelectListItem() { Value="3", Text= "3" },
+             new SelectListItem() { Value="6", Text= "6" },
+             new SelectListItem() { Value="12", Text= "12" },
+             new SelectListItem() { Value="24", Text= "24" },
+             new SelectListItem() { Value="10000000", Text= "All" },
+            };
+
+            ViewBag.CurrentPageSize = pSize;
+
+            return View(kitchens.ToPagedList(pageNumber, pSize));
         }
 
         [HttpGet]
