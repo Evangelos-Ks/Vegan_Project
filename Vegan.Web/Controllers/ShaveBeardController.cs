2d39e1eae38d3ba6902782eb12897e3907f82136
Evangelos Koutsogiorgos <evangkouts@gmail.com>
2020-09-24 16:17:52 +0100

    ShaveBeard IndexUser & ActionMethod -> ready

--- Vegan.Web/Controllers/ShaveBeardController.cs
+++ Vegan.Web/Controllers/ShaveBeardController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Web.Mvc;
 using Vegan.Database;
 using Vegan.Entities.Care;
@@ -11,17 +14,72 @@ namespace Vegan.Web.Controllers.TestControllers
         //===================================== Fields =====================================================================
         private UnitOfWork unitOfWork = new UnitOfWork(new MyDatabase());
 
-        //===================================== Methods ====================================================================
+        //===================================== Action Methods =============================================================
         [HttpGet]
-     // [Authorize(Roles = "Admins, Supervisors")]
         public ActionResult Index()
         {
             return View(unitOfWork.ShaveBeards.GetAll());
         }
+
         [HttpGet]
-        public ActionResult IndexUser()
+        public ActionResult IndexUser(string sortOrder, int? minPrice, int? maxPrice, int? page, int? pageSize)
         {
-            return View(unitOfWork.ShaveBeards.GetAll());
+            //Get all shaveBeards products
+            IEnumerable<ShaveBeard> shaveBeards = unitOfWork.ShaveBeards.GetAll().ToList();
+            unitOfWork.Dispose();
+
+            //Filter
+            ViewBag.MinPrice = minPrice;
+            ViewBag.MaxPrice = maxPrice;
+
+            if (minPrice != null)
+            {
+                shaveBeards = shaveBeards.Where(c => c.Price >= minPrice);
+            }
+
+            if (maxPrice != null)
+            {
+                shaveBeards = shaveBeards.Where(c => c.Price <= maxPrice);
+            }
+
+            //Sorting
+            ViewBag.TitleSortParam = string.IsNullOrEmpty(sortOrder) ? "title_desc" : "";
+            ViewBag.PriceSortParam = sortOrder == "price_asc" ? "price_desc" : "price_asc";
+
+            switch (sortOrder)
+            {
+                case "title_desc":
+                    shaveBeards = shaveBeards.OrderByDescending(c => c.Title);
+                    break;
+                case "price_asc":
+                    shaveBeards = shaveBeards.OrderBy(c => c.Price);
+                    break;
+                case "price_desc":
+                    shaveBeards = shaveBeards.OrderByDescending(c => c.Price);
+                    break;
+                default:
+                    shaveBeards = shaveBeards.OrderBy(c => c.Title);
+                    break;
+            }
+
+            //Paging
+            ViewBag.CurrentSort = sortOrder;
+
+            int pSize = pageSize ?? 6;
+            int pageNumber = page ?? 1;
+
+            ViewBag.PageSize = new List<SelectListItem>()
+            {
+             new SelectListItem() { Value="3", Text= "3" },
+             new SelectListItem() { Value="6", Text= "6" },
+             new SelectListItem() { Value="12", Text= "12" },
+             new SelectListItem() { Value="24", Text= "24" },
+             new SelectListItem() { Value="10000000", Text= "All" },
+            };
+
+            ViewBag.CurrentPageSize = pSize;
+
+            return View(shaveBeards.ToPagedList(pageNumber, pSize));
         }
 
         [HttpGet]
