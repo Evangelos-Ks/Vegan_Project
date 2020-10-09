d4cc42051b7f3dc65732428f5c6f118846251232
Evangelos Koutsogiorgos <evangkouts@gmail.com>
2020-10-09 18:16:19 +0100

    User view from admin redirect to Details of products

--- Vegan.Web/Controllers/ProductController.cs
+++ Vegan.Web/Controllers/ProductController.cs
@@ -2,6 +2,7 @@
 using System.Web.Mvc;
 using Vegan.Database;
 using Vegan.Entities;
+using Vegan.Entities.Care;
 using Vegan.Services;
 
 namespace Vegan.Web.Controllers.TestControllers
@@ -12,7 +13,7 @@ namespace Vegan.Web.Controllers.TestControllers
         private UnitOfWork unitOfWork = new UnitOfWork(new MyDatabase());
 
         //===================================== Methods ====================================================================
-       
+
         [HttpGet]
         [Authorize(Roles = "Admins")]
         public ActionResult Index()
@@ -56,6 +57,93 @@ namespace Vegan.Web.Controllers.TestControllers
 
         public ActionResult DetailsProduct(int productId)
         {
+            string actionMethod = "Details";
+            if (unitOfWork.Candles.GetById(productId) != null)
+            {
+                var product = unitOfWork.Candles.GetById(productId);
+                actionMethod = actionMethod + product.SubCategory;
+                return RedirectToAction(actionMethod, product.SubCategory, new { productId });
+            }
+            if (unitOfWork.EssentialOils.GetById(productId) != null)
+            {
+                var product = unitOfWork.EssentialOils.GetById(productId);
+                actionMethod = actionMethod + product.SubCategory;
+                return RedirectToAction(actionMethod, product.SubCategory, new { productId });
+            }
+            if (unitOfWork.HomeCleanings.GetById(productId) != null)
+            {
+                var product = unitOfWork.HomeCleanings.GetById(productId);
+                actionMethod = actionMethod + product.SubCategory;
+                return RedirectToAction(actionMethod, product.SubCategory, new { productId });
+            }
+            if (unitOfWork.Kitchens.GetById(productId) != null)
+            {
+                var product = unitOfWork.Kitchens.GetById(productId);
+                actionMethod = actionMethod + product.SubCategory;
+                return RedirectToAction(actionMethod, product.SubCategory, new { productId });
+            }
+            if (unitOfWork.Salts.GetById(productId) != null)
+            {
+                var product = unitOfWork.Salts.GetById(productId);
+                actionMethod = actionMethod + product.SubCategory;
+                return RedirectToAction(actionMethod, product.SubCategory, new { productId });
+            }
+            if (unitOfWork.Spices.GetById(productId) != null)
+            {
+                var product = unitOfWork.Spices.GetById(productId);
+                actionMethod = actionMethod + product.SubCategory;
+                return RedirectToAction(actionMethod, product.SubCategory, new { productId });
+            }
+            if (unitOfWork.SproutingSeeds.GetById(productId) != null)
+            {
+                var product = unitOfWork.SproutingSeeds.GetById(productId);
+                actionMethod = actionMethod + product.SubCategory;
+                return RedirectToAction(actionMethod, product.SubCategory, new { productId });
+            }
+            if (unitOfWork.Teas.GetById(productId) != null)
+            {
+                var product = unitOfWork.Teas.GetById(productId);
+                actionMethod = actionMethod + product.SubCategory;
+                return RedirectToAction(actionMethod, product.SubCategory, new { productId });
+            }
+            if (unitOfWork.PowerHealths.GetById(productId) != null)
+            {
+                var product = unitOfWork.PowerHealths.GetById(productId);
+                actionMethod = actionMethod + product.SubCategory;
+                return RedirectToAction(actionMethod, product.SubCategory, new { productId });
+            }
+            if (unitOfWork.SuperFoods.GetById(productId) != null)
+            {
+                var product = unitOfWork.SuperFoods.GetById(productId);
+                actionMethod = actionMethod + product.SubCategory;
+                return RedirectToAction(actionMethod, product.SubCategory, new { productId });
+            }
+            if (unitOfWork.FaceCreams.GetById(productId) != null)
+            {
+                var product = unitOfWork.FaceCreams.GetById(productId);
+                actionMethod = actionMethod + product.SubCategory;
+                return RedirectToAction(actionMethod, product.SubCategory + "s", new { productId });
+            }
+            if (unitOfWork.Hairs.GetById(productId) != null)
+            {
+                var product = unitOfWork.Hairs.GetById(productId);
+                actionMethod = actionMethod + product.SubCategory;
+                return RedirectToAction(actionMethod, product.SubCategory, new { productId });
+            }
+            if (unitOfWork.Lotions.GetById(productId) != null)
+            {
+                var product = unitOfWork.Lotions.GetById(productId);
+                actionMethod = actionMethod + product.SubCategory;
+                return RedirectToAction(actionMethod, product.SubCategory, new { productId });
+            }
+            if (unitOfWork.ShaveBeards.GetById(productId) != null)
+            {
+                var product = unitOfWork.ShaveBeards.GetById(productId);
+                actionMethod = actionMethod + product.SubCategory;
+                return RedirectToAction(actionMethod, product.SubCategory, new { productId });
+            }
+
+
             return View(unitOfWork.Products.GetById(productId));
         }
 
